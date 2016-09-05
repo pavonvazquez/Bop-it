b10426992ed1bc274f6d702f40ad9d332ad8b555
Arturo Pavón <ingcpavon@gmail.com>
2016-09-04 22:16:12 -0500

    Solo falta fiesta

--- bop.py
+++ bop.py
@@ -191,7 +191,7 @@ def solo_master():
 	correcto = True
 	inicio = time.time()
 	chido=0
-	while time.time()-inicio< 3 and chido<3:
+	while time.time()-inicio< 1 and chido<3:
 		time.sleep(1)
 		i = randint(0,8)
 		if 0<=i<=5:
@@ -301,8 +301,131 @@ def solo_master():
 	return chido
 
 
+def multi_master():
+	ins = ["Gira","Golpea","Jala","1","2","3","Pasala",(255,255,0),(255,255,255),(0,0,255)]
+	pygame.init()
+	ventana = pygame.display.set_mode((800,600))
+	pygame.display.set_caption("Bop it")
+	reloj = pygame.time.Clock()
+	correcto = True
+	inicio = time.time()
+	chido=0
+	while time.time()-inicio< 1 and chido<3:
+		time.sleep(1)
+		i = randint(0,9)
+		if 0<=i<=6:
+			display_mensaje(ins[i],ventana)
+			ventana.fill((0,0,0))
+			if(i==2):
+				for event in pygame.event.get():
+					if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+					"""else:
+						display_mensaje("Ya valiste",ventana)
+						ventana.fill((0,0,0))
+						correcto = False"""
+			if(i==1):		
+				for event in pygame.event.get():
+					if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+					"""else:
+						display_mensaje("Ya valiste",ventana)
+						ventana.fill((0,0,0))
+						correcto = False"""
+			if(i==0):
+				for event in pygame.event.get():
+					if event.type == pygame.MOUSEBUTTONDOWN and (event.button == 4 or event.button==5):
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+					"""else:
+						display_mensaje("Ya valiste",ventana)
+						ventana.fill((0,0,0))
+						correcto = False"""
+			if(i==3):
+				for event in pygame.event.get():
+					if event.type == pygame.MOUSEBUTTONDOWN and (event.button == 4 or event.button==5):
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+			if(i==4):
+				for event in pygame.event.get():
+					if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+			if(i==5):
+				for event in pygame.event.get():
+					if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+			if(i==6):
+				for event in pygame.event.get():
+					if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+		else:
+			ventana.fill(ins[i])
+			pygame.display.update()
+			time.sleep(1)
+			ventana.fill((0,0,0))
+			pygame.display.update()
+			if(i==7):
+				
+				for event in pygame.event.get():
+					if event.type == pygame.MOUSEBUTTONDOWN and (event.button == 4 or event.button==5):
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+			if(i==8):
+				for event in pygame.event.get():
+					if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
+			if(i==9):
+				for event in pygame.event.get():
+					if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+						ventana.fill((0,0,0))
+						pygame.display.update()
+						display_mensaje("Correcto",ventana)
+						ventana.fill((0,0,0))
+						correcto = True
+						inicio = time.time()
+						chido+=1
 
-
+	if chido>=3:
+		print("Felicidades")
+	else:
+		display_mensaje("Ya valiste",ventana)
+		ventana.fill((0,0,0))
+	pygame.display.quit()
+	pygame.quit()
+	return chido
 def solo_experto():
 	ins = ["Gira","Golpea","Jala","1","2","3"]
 	pygame.init()
